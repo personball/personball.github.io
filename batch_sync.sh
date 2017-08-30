979fd6393a4f00acb1432989a3df1f828096ddca
personball <personball@163.com>
2017-08-30 23:58:24 +0800

    add comment to batch_sync.sh

--- batch_sync.sh
+++ batch_sync.sh
@@ -1,3 +1,5 @@
+
+# created by `find _posts/*|xargs -n 1 echo ./post_sync.rb >> batch_sync.sh`
 ./post_sync.rb _posts/2017-06-04-tips-of-identity-server-4-application.md
 sleep 55s
 ./post_sync.rb _posts/2017-07-09-rebus-handler-idempotence.md
